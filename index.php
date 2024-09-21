b558b2edf4f2bf6b65ce5d46f49370ea83016328
Ralph Joedel <joedel.rf@gmail.com>
2024-09-21 10:03:38 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+echo 'PHP 9/21/2024 Practice' ."<br>";
+
+$num = 1;
+$defaultNumValue = 10;
+$defaultStringValue = 'NULL String';
+$nLine = "<br>";
+echo $defaultStringValue .$nLine;
+echo $nLine;
+
+echo 'Function and Arrays' .$nLine;
+
+$defaultArray = [1, 2, 3, 4];
+
+function arrayData($arr1, $arr2, $arr3){
+    return $arrays = [$arr1, $arr2, $arr3];
+    
+};
+foreach(arrayData(1, 2, 3) as $arr){
+    echo $arr ."<br>";
+}
+
+function checkArray($count){
+    echo $count == 3 ? 'Array Valid Count' : 'Array Not Valid' ;
+
+}
+
+checkArray(4);
+
+
+?>
